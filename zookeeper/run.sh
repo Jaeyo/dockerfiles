0ac98149e2ea2a7d9299d39371e61206d8d3bd8c
이재용 <lastiverse@gmail.com>
2017-10-19 16:21:39 +0900

    initial

--- zookeeper/run.sh
+++ zookeeper/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -td --name zookeeper -p 2181:2181 -v /home/ubuntu/zookeeper_container/data/:/usr/local/zookeeper-3.4.10/data/ zookeeper
+docker run -td --name zookeeper -p 2181:2181 zookeeper
